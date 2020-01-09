e0efb82edd59b7b7b1b8cfa7e5a13817f5a49183
MyraMidnight <dark_stardragon@hotmail.com>
2020-01-09 10:03:31 +0000

    skeleton of main.py as comments

---
+++ main.py
@@ -0,0 +1,11 @@
+#import 'session' class from /main
+
+#Give user option to start a new session
+
+#While user does not 'end' the session, allow them to do other things
+#takes in commands to do other things (example: print current time duration)
+
+#if 'end', then show current time
+#put the data from ended session into a list (for history)
+
+#let user create a new session
