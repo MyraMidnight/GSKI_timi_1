222f9f8daa7199cb5711ffac7b2867561dba86d6
Nanna Katrín Hannesdóttir <Nanna@Nannas-MacBook-Air-4.local>
2020-01-09 10:51:56 +0000

    Session klasi

---
+++ class_session.py
@@ -0,0 +1,19 @@
+import time
+
+class session():
+
+    def __init__:
+        self.__time = time.time()
+    
+    def get_time(self):
+        time = self.__time - time.(time)
+        return time
+
+    def end_timer(self):
+        print(self.get_time())
+
+
+
+
+
+
