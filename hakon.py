3d5e68ac8c3442c3597f723e21bb7ec5e9805e78
Konn1nn <54936310+Konn1nn@users.noreply.github.com>
2020-01-09 11:11:49 +0000

    Create hakon.py

---
+++ hakon.py
@@ -0,0 +1,22 @@
+import time
+
+def time_start():
+    return time.time_ns()
+
+def time_end(start_var):
+    end_var = time.time_ns()
+    diff_var = end_var - start_var
+    print(diff_var, " nano seconds")
+    seconds = diff_var/(10**9)
+    minutes = seconds // 60
+    print(seconds, " seconds")
+    print(minutes, " minutes")
+
+
+start = time_start()
+
+my_var = 1
+while my_var < 2000000000:
+    my_var += 1
+
+time_end(start)
